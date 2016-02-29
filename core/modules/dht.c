0216f5313fc380af4119c4a50d75d64e0caae8fa
Aurojit Panda <apanda@cs.berkeley.edu>
2016-02-29 09:29:28 -0800

    Load balancer

--- core/modules/dht.c
+++ core/modules/dht.c
@@ -3,26 +3,25 @@
 #include "../utils/simd.h"
 #include "flowtable.h"
 
-/******************************************************************************/
-
 struct dht_priv {
 	int init;
 	struct flow_table flow_table;
 	gate_t default_gate;
 };
 
-static struct snobj *dht_forward_init(struct module *m, struct snobj *arg)
+static struct snobj *dht_init(struct module *m, struct snobj *arg)
 {
 	struct dht_priv *priv = get_priv(m);
 	int ret = 0;
 	int size = snobj_eval_int(arg, "size");
 	int bucket = snobj_eval_int(arg, "bucket");
+	int default_gate = snobj_eval_int(arg, "default_gate");
 
 	assert(priv != NULL);
 
 	priv->init = 0;
 
-	priv->default_gate = INVALID_GATE;
+	priv->default_gate = default_gate;
 
 	if (size == 0)
 		size = DEFAULT_TABLE_SIZE;
@@ -43,7 +42,7 @@ static struct snobj *dht_forward_init(struct module *m, struct snobj *arg)
 	return NULL;
 }
 
-static void dht_forward_deinit(struct module *m)
+static void dht_deinit(struct module *m)
 {
 	struct dht_priv *priv = get_priv(m);
 
@@ -53,91 +52,14 @@ static void dht_forward_deinit(struct module *m)
 	}
 }
 
-static inline struct snobj*
-flow_from_snobj(struct snobj *entry, struct flow* flow) {
-	if (snobj_type(entry) != TYPE_MAP) {
-		return snobj_err(EINVAL,
-				 "add must be given as a list of map");
-	}
-
-	struct snobj *src_addr = snobj_map_get(entry, "src_addr");
-	struct snobj *dst_addr = snobj_map_get(entry, "dst_addr");
-	struct snobj *src_port = snobj_map_get(entry, "src_port");
-	struct snobj *dst_port = snobj_map_get(entry, "dst_port");
-	struct snobj *protocol = snobj_map_get(entry, "protocol");
-
-	struct snobj *gate = snobj_map_get(entry, "gate");
-
-	if (!src_addr || snobj_type(src_addr) != TYPE_INT)
-		return snobj_err(EINVAL, "Must supply source address");
-
-	if (!dst_addr || snobj_type(dst_addr) != TYPE_INT)
-		return snobj_err(EINVAL, 
-				"Must supply destination address");
-
-	if (!src_port || snobj_type(src_port) != TYPE_INT ||
-			snobj_int_get(src_port) > UINT16_MAX)
-		return snobj_err(EINVAL, 
-				"Must supply valid source port");
-
-	if (!dst_port || snobj_type(dst_port) != TYPE_INT ||
-			snobj_int_get(dst_port) > UINT16_MAX)
-		return snobj_err(EINVAL,
-				"Must supply valid destination port");
-
-	if (!protocol || snobj_type(protocol) != TYPE_INT ||
-			snobj_int_get(protocol) > UINT8_MAX)
-		return snobj_err(EINVAL,
-				"Must supply valid protocol");
-
-	if (!gate || snobj_type(gate) != TYPE_INT ||
-			snobj_int_get(gate) >= INVALID_GATE)
-		return snobj_err(EINVAL,
-				"Must supply valid gate");
-
-
-	flow->src_addr = (uint32_t) snobj_int_get(src_addr);
-	flow->dst_addr = (uint32_t) snobj_int_get(dst_addr);
-	flow->src_port = (uint16_t) snobj_int_get(src_port);
-	flow->dst_port = (uint16_t) snobj_int_get(dst_port);
-	flow->protocol = (uint8_t) snobj_int_get(protocol);
-	return NULL;
-}
-
-static inline int 
-extract_flow(struct snbuf *snb, struct flow *flow)
+int dht_add_flow(struct module* m, struct flow *flow, gate_t gate)
 {
-	struct ether_hdr *eth;
-	struct ipv4_hdr *ip;
-	struct udp_hdr *udp;
-
-	eth = (struct ether_hdr*)snb_head_data(snb);
-
-	if (eth->ether_type != rte_cpu_to_be_16(ETHER_TYPE_IPv4))
-		return -1;
-
-	ip = (struct ipv4_hdr *)(eth + 1);
-
-	int ihl = (ip->version_ihl & IPV4_HDR_IHL_MASK) *
-		IPV4_IHL_MULTIPLIER;
-
-	if (ip->next_proto_id != 17 &&
-	    ip->next_proto_id != 6)
-		return -1;
-	
-	udp = (struct udp_hdr*)(((char*)ip) + ihl);
-
-	flow->src_addr = ip->src_addr;
-	flow->dst_addr = ip->dst_addr;
-	flow->src_port = udp->src_port;
-	flow->dst_port = udp->dst_port;
-	flow->protocol = ip->next_proto_id;
-	
-	return 0;
+	struct dht_priv *priv = get_priv(m);
+	return ftb_add_entry(&priv->flow_table, flow, gate);
 }
 
-static struct snobj *handle_add(struct dht_priv *priv,
-				struct snobj *add)
+static struct snobj 
+*handle_add(struct dht_priv *priv, struct snobj *add)
 {
 	int i;
 
@@ -256,7 +178,7 @@ static struct snobj *handle_def_gate(struct dht_priv *priv,
 	return NULL;
 }
 
-static struct snobj *dht_forward_query(struct module *m, struct snobj *q)
+static struct snobj *dht_query(struct module *m, struct snobj *q)
 {
 	struct dht_priv *priv = get_priv(m);
 
@@ -294,14 +216,13 @@ static struct snobj *dht_forward_query(struct module *m, struct snobj *q)
 	return NULL;
 }
 
-static struct snobj *dht_forward_get_desc(const struct module *m)
+static struct snobj *dht_get_desc(const struct module *m)
 {
 	return NULL;
 }
 
-
 __attribute__((optimize("unroll-loops")))
-static void dht_forward_process_batch(struct module *m, struct pkt_batch *batch)
+static void dht_process_batch(struct module *m, struct pkt_batch *batch)
 {
 	gate_t ogates[MAX_PKT_BURST];
 	int r, i;
@@ -322,27 +243,21 @@ static void dht_forward_process_batch(struct module *m, struct pkt_batch *batch)
 			r = ftb_find(&priv->flow_table,
 				     &flow,
 				     &ogates[i]);
-			/* Could not find where to send */
-			if (r != 0) {
-				ogates[i] = priv->default_gate;
-			}
 		}
 	}
 
 	run_split(m, ogates, batch);
 }
 
-
 static const struct mclass e2_dht = {
 	.name            = "DHT",
 	.def_module_name = "dht",
 	.priv_size       = sizeof(struct dht_priv),
-	.init            = dht_forward_init,
-	.deinit          = dht_forward_deinit,
-	.query           = dht_forward_query,
-	.get_desc        = dht_forward_get_desc,
-	.process_batch   = dht_forward_process_batch,
+	.init            = dht_init,
+	.deinit          = dht_deinit,
+	.query           = dht_query,
+	.get_desc        = dht_get_desc,
+	.process_batch   = dht_process_batch,
 };
-
 
 ADD_MCLASS(e2_dht)
--- core/modules/flowtable.h
+++ core/modules/flowtable.h
@@ -377,4 +377,87 @@ static uint64_t ftb_addr_to_u64(char* addr)
 	return  (*addrp & 0x0000FFffFFffFFfflu);
 }
 
+static inline struct snobj*
+flow_from_snobj(struct snobj *entry, struct flow* flow) {
+	if (snobj_type(entry) != TYPE_MAP) {
+		return snobj_err(EINVAL,
+				 "add must be given as a list of map");
+	}
+
+	struct snobj *src_addr = snobj_map_get(entry, "src_addr");
+	struct snobj *dst_addr = snobj_map_get(entry, "dst_addr");
+	struct snobj *src_port = snobj_map_get(entry, "src_port");
+	struct snobj *dst_port = snobj_map_get(entry, "dst_port");
+	struct snobj *protocol = snobj_map_get(entry, "protocol");
+
+	struct snobj *gate = snobj_map_get(entry, "gate");
+
+	if (!src_addr || snobj_type(src_addr) != TYPE_INT)
+		return snobj_err(EINVAL, "Must supply source address");
+
+	if (!dst_addr || snobj_type(dst_addr) != TYPE_INT)
+		return snobj_err(EINVAL, 
+				"Must supply destination address");
+
+	if (!src_port || snobj_type(src_port) != TYPE_INT ||
+			snobj_int_get(src_port) > UINT16_MAX)
+		return snobj_err(EINVAL, 
+				"Must supply valid source port");
+
+	if (!dst_port || snobj_type(dst_port) != TYPE_INT ||
+			snobj_int_get(dst_port) > UINT16_MAX)
+		return snobj_err(EINVAL,
+				"Must supply valid destination port");
+
+	if (!protocol || snobj_type(protocol) != TYPE_INT ||
+			snobj_int_get(protocol) > UINT8_MAX)
+		return snobj_err(EINVAL,
+				"Must supply valid protocol");
+
+	if (!gate || snobj_type(gate) != TYPE_INT ||
+			snobj_int_get(gate) >= INVALID_GATE)
+		return snobj_err(EINVAL,
+				"Must supply valid gate");
+
+
+	flow->src_addr = (uint32_t) snobj_int_get(src_addr);
+	flow->dst_addr = (uint32_t) snobj_int_get(dst_addr);
+	flow->src_port = (uint16_t) snobj_int_get(src_port);
+	flow->dst_port = (uint16_t) snobj_int_get(dst_port);
+	flow->protocol = (uint8_t) snobj_int_get(protocol);
+	return NULL;
+}
+
+static inline int 
+extract_flow(struct snbuf *snb, struct flow *flow)
+{
+	struct ether_hdr *eth;
+	struct ipv4_hdr *ip;
+	struct udp_hdr *udp;
+
+	eth = (struct ether_hdr*)snb_head_data(snb);
+
+	if (eth->ether_type != rte_cpu_to_be_16(ETHER_TYPE_IPv4))
+		return -1;
+
+	ip = (struct ipv4_hdr *)(eth + 1);
+
+	int ihl = (ip->version_ihl & IPV4_HDR_IHL_MASK) *
+		IPV4_IHL_MULTIPLIER;
+
+	if (ip->next_proto_id != 17 &&
+	    ip->next_proto_id != 6)
+		return -1;
+	
+	udp = (struct udp_hdr*)(((char*)ip) + ihl);
+
+	flow->src_addr = ip->src_addr;
+	flow->dst_addr = ip->dst_addr;
+	flow->src_port = udp->src_port;
+	flow->dst_port = udp->dst_port;
+	flow->protocol = ip->next_proto_id;
+	
+	return 0;
+}
+
 #endif
