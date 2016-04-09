224981501e4f81c9cf0b9c0479e7aed3258ec51a
Aurojit Panda <apanda@cs.berkeley.edu>
2016-04-08 18:54:57 -0700

    Upgrade DPDK and fix vport_rte_ring a bit.

--- core/dpdk.h
+++ core/dpdk.h
@@ -8,11 +8,13 @@
 #include <rte_version.h>
 
 #ifndef RTE_VER_MAJOR
-  #error DPDK version is not available
-#else
+  //#error DPDK version is not available
+  #define RTE_VER_MAJOR 3
+  #define RTE_VER_PATCH_LEVEL 0
+  #define RTE_VER_PATCH_RELEASE 0
+#endif
   #define DPDK_VER(a,b,c) (((a << 16) | (b << 8) | (c)))
   #define DPDK DPDK_VER(RTE_VER_MAJOR,RTE_VER_MINOR,RTE_VER_PATCH_LEVEL)
-#endif
 
 void init_dpdk(char *prog_name);
 
--- core/drivers/vport_rte_ring.c
+++ core/drivers/vport_rte_ring.c
@@ -5,8 +5,8 @@
 #include <fcntl.h>
 #include <unistd.h>
 
-#include <rte_malloc.h>
 #include <rte_config.h>
+#include <rte_malloc.h>
 #include <rte_ring.h>
 #include <rte_errno.h>
 #include <rte_eth_ring.h>
@@ -32,17 +32,9 @@
 #define SINGLE_C		0
 
 #define VPORT_DIR_PREFIX "sn_vports"
-#define RTE_RING_NAME "vport_ring"
+#define RTE_RING_NAME "ETH_RXTX"
 
-struct vport_inc_regs {
-	uint64_t dropped;
-} __cacheline_aligned;
-
-struct vport_out_regs {
-	uint32_t irq_enabled;
-} __cacheline_aligned;
-
-/* This is equivalent to the old bar */
+/* This is sort of equivalent to the old bar */
 struct rte_ring_bar {
 	char name[PORT_NAME_LEN];
 
@@ -53,23 +45,18 @@ struct rte_ring_bar {
 	int num_inc_q;
 	int num_out_q;
 
-	struct vport_inc_regs* inc_regs[MAX_QUEUES_PER_DIR];
 	struct rte_ring* inc_qs[MAX_QUEUES_PER_DIR];
 
-	struct vport_out_regs* out_regs[MAX_QUEUES_PER_DIR];
 	struct rte_ring* out_qs[MAX_QUEUES_PER_DIR];
 };
 
 struct vport_priv {
 	struct rte_ring_bar* bar;
 
-	struct vport_inc_regs* inc_regs[MAX_QUEUES_PER_DIR];
 	struct rte_ring* inc_qs[MAX_QUEUES_PER_DIR];
 
-	struct vport_out_regs* out_regs[MAX_QUEUES_PER_DIR];
 	struct rte_ring* out_qs[MAX_QUEUES_PER_DIR];
 
-	int out_irq_fd[MAX_QUEUES_PER_DIR];
 	int port;
 };
 
@@ -81,16 +68,14 @@ static struct snobj *vport_init_port(struct port *p, struct snobj *arg)
 	int num_inc_q = p->num_queues[PACKET_DIR_INC];
 	int num_out_q = p->num_queues[PACKET_DIR_OUT];
 
-	int bytes_per_ring;
 	int total_bytes;
-	uint8_t *ptr;
 	int i;
 	char port_dir[PORT_NAME_LEN + 256];
 	char file_name[PORT_NAME_LEN + 256];
+	char ring_name[PORT_NAME_LEN + 256];
 	struct stat sb;
 	FILE* fp;
 	size_t bar_address;
-	struct rte_ring *ring = NULL;
 	int ret;
 	struct rte_eth_conf null_conf;
 	int numa_node = 0;
@@ -101,11 +86,8 @@ static struct snobj *vport_init_port(struct port *p, struct snobj *arg)
 
 	memset(&null_conf, 0, sizeof(struct rte_eth_conf));
 
-	bytes_per_ring = rte_ring_get_memsize(SLOTS_PER_LLRING);
-	total_bytes =	sizeof(struct rte_ring_bar) +
-			(bytes_per_ring * (num_inc_q + num_out_q)) +
-			(sizeof(struct vport_inc_regs) * (num_inc_q)) +
-			(sizeof(struct vport_out_regs) * (num_out_q));
+	/*bytes_per_ring = rte_ring_get_memsize(SLOTS_PER_LLRING);*/
+	total_bytes =	sizeof(struct rte_ring_bar);
 
 	bar = rte_zmalloc(NULL, total_bytes, 0);
 	bar_address = (size_t)bar;
@@ -116,48 +98,40 @@ static struct snobj *vport_init_port(struct port *p, struct snobj *arg)
 	bar->num_inc_q = num_inc_q;
 	bar->num_out_q = num_out_q;
 
-	ptr = (uint8_t*)(bar + 1);
-
 	/* Set up inc rte_rings */
 	for (i = 0; i < num_inc_q; i++) {
-		priv->inc_regs[i] = bar->inc_regs[i] =
-			(struct vport_inc_regs*)ptr;
-		ptr += sizeof(struct vport_inc_regs);
-		ret = rte_ring_init((struct rte_ring*)ptr, 
-				RTE_RING_NAME, SLOTS_PER_LLRING, 
+		snprintf(ring_name, PORT_NAME_LEN + 256, 
+				"%s%s%d", p->name, "incq", i);
+		bar->inc_qs[i] = rte_ring_create(ring_name,
+				SLOTS_PER_LLRING,
+				numa_node,
 				SINGLE_P | SINGLE_C);
-		if (ret != 0) {
+		if (bar->inc_qs[i] == NULL) {
 			return snobj_err(rte_errno, "Failed to allocate ring");
 		}
-		ring = (struct rte_ring*)ptr;
-		if (rte_ring_set_water_mark(ring, SLOTS_WATERMARK) != 0) {
+		priv->inc_qs[i] = bar->inc_qs[i];
+		if (rte_ring_set_water_mark(priv->inc_qs[i],
+					SLOTS_WATERMARK) != 0) {
 			return snobj_err(EINVAL, "Could not set watermark");
 		}
-		bar->inc_qs[i] = ring;
-		priv->inc_qs[i] = bar->inc_qs[i];
-		ptr += bytes_per_ring;
 	}
 
 	/* Set up out rte_rings */
 	for (i = 0; i < num_out_q; i++) {
-		priv->out_regs[i] = bar->out_regs[i] =
-			(struct vport_out_regs*)ptr;
-		ptr += sizeof(struct vport_out_regs);
-
-		ret = rte_ring_init((struct rte_ring*)ptr, 
-				RTE_RING_NAME, SLOTS_PER_LLRING, 
+		snprintf(ring_name, PORT_NAME_LEN + 256, 
+				"%s%s%d", p->name, "outq", i);
+		bar->out_qs[i] = rte_ring_create(ring_name,
+				SLOTS_PER_LLRING,
+				numa_node,
 				SINGLE_P | SINGLE_C);
-		if (ret != 0) {
+		if (bar->out_qs[i] == NULL) {
 			return snobj_err(rte_errno, "Failed to allocate ring");
 		}
-		ring = (struct rte_ring*)ptr;
-		if (rte_ring_set_water_mark(ring, SLOTS_WATERMARK) != 0) {
+		priv->out_qs[i] = bar->out_qs[i];
+		if (rte_ring_set_water_mark(priv->out_qs[i],
+					SLOTS_WATERMARK) != 0) {
 			return snobj_err(EINVAL, "Could not set watermark");
 		}
-
-		bar->out_qs[i] = ring;
-		priv->out_qs[i] = bar->out_qs[i];
-		ptr += bytes_per_ring;
 	}
 
 	snprintf(port_dir, PORT_NAME_LEN + 256, "%s/%s",
@@ -168,15 +142,6 @@ static struct snobj *vport_init_port(struct port *p, struct snobj *arg)
 	} else {
 		log_info("Creating directory %s\n", port_dir);
 		mkdir(port_dir, S_IRWXU | S_IRWXG | S_IRWXO);
-	}
-
-	for (i = 0; i < num_out_q; i++) {
-		snprintf(file_name, PORT_NAME_LEN + 256, "%s/%s/%s.rx%d",
-				P_tmpdir, VPORT_DIR_PREFIX, p->name, i);
-
-		mkfifo(file_name, 0666);
-
-		priv->out_irq_fd[i] = open(file_name, O_RDWR);
 	}
 
 	snprintf(file_name, PORT_NAME_LEN + 256, "%s/%s/%s",
@@ -197,6 +162,7 @@ static struct snobj *vport_init_port(struct port *p, struct snobj *arg)
 		return snobj_err(EINVAL, "Could not configure port");
 	}
 
+	/* Set up both RX and TX cores */
 	for (i = 0; i < num_inc_q; i++) {
 		int sid = numa_node;
 
@@ -210,7 +176,7 @@ static struct snobj *vport_init_port(struct port *p, struct snobj *arg)
 	}
 
 	for (i = 0; i < num_out_q; i++) {
-		int sid = 0;		/* XXX */
+		int sid = numa_node;
 
 		ret = rte_eth_tx_queue_setup(priv->port, i,
 					     32,
@@ -228,16 +194,7 @@ static void vport_deinit_port(struct port *p)
 	struct vport_priv *priv = get_port_priv(p);
 	char file_name[PORT_NAME_LEN + 256];
 
-	int num_out_q = p->num_queues[PACKET_DIR_OUT];
 	rte_eth_dev_stop(priv->port);
-
-	for (int i = 0; i <num_out_q; i++) {
-		snprintf(file_name, PORT_NAME_LEN + 256, "%s/%s/%s.rx%d",
-				P_tmpdir, VPORT_DIR_PREFIX, p->name, i);
-		
-		unlink(file_name);
-		close(priv->out_irq_fd[i]);
-	}
 
 	snprintf(file_name, PORT_NAME_LEN + 256, "%s/%s/%s",
 			P_tmpdir, VPORT_DIR_PREFIX, p->name);
@@ -266,7 +223,7 @@ vport_recv_pkts(struct port *p, queue_t qid, snb_array_t pkts, int cnt)
 
 static const struct driver ring_port = {
 	.name 		= "RteRingVPort",
-	.def_port_name	= "bessvp",
+	.def_port_name	= "rte_ring",
 	.priv_size	= sizeof(struct vport_priv),
 	.init_port 	= vport_init_port,
 	.recv_pkts 	= vport_recv_pkts,
